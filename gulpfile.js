3bd5f59ba7f62a27d13ce511475a6cf43057eb0b
leafgg <jobs.lugui@gmail.com>
2024-02-29 09:06:56 -0300

    feat: initial commit with landing layout

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask(){
+  return src('src/scss/main.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask(){
+  return src('src/js/*.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(concat('script.js'))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['src/scss/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        slidesToShow: 1,
+        centerMode: true,
+        arrows: true,
+        dots: true,
+        fade: true,
+    })
+})
